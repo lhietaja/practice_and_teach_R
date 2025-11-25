97d4a89163761d5b9b6e889c5576d8ae08b26912
Hietajärvi <lhietaja@ad.helsinki.fi>
2025-11-25 13:20:02 +0200

    J-N update

--- moderation.R
+++ moderation.R
@@ -6,14 +6,25 @@ library(modsem)
 #https://modsem.org/articles/modsem.html
 
 head(oneInt)
+oneInt2 <- oneInt %>% 
+  mutate(x1 = 0.5*x1 + rnorm(2000),
+         x2 = 0.6*x2 + rnorm(2000),
+         x3 = 0.6*x3 + rnorm(2000),
+         z1 = 0.5*z1 + rnorm(2000),
+         z2 = 0.6*z2 + rnorm(2000),
+         z3 = 0.7*z3 + rnorm(2000),
+         y1 = 0.7*y1 + rnorm(2000),
+         y2 = 0.6*y2 + rnorm(2000),
+         y3 = 0.5*y3 + rnorm(2000))
 
-## simple approach
+
+## simple approach ----
 
 ### specify model
 model <- '
   # Measurement model
   X =~ x1 + x2 + x3  
-  Z =~ z1 + z2 + z3  
+  Z =~ z1 + z2 + z3
   Y =~ y1 + y2 + y3  
 
   # Structural model
@@ -22,11 +33,9 @@ model <- '
 
 ### fit basic model and examine
 fit <- modsem(model, data = oneInt)
-
 summary(fit, fit.measures = TRUE, standardized = TRUE)
 
 ### fit LMS model and examine
-
 fit2 <- modsem(model, method = "lms", data = oneInt)
 summary(fit2, fit.measures = TRUE, standardized = TRUE)
 
@@ -56,6 +65,7 @@ interactions %>%
   scale_y_discrete(limits=rev) +
   theme_classic()
 
+### classic plots
 plot_interaction(x = "X", z = "Z", y = "Y",
                  vals_z = c(-1,0,1), model = fit)
 
@@ -63,13 +73,19 @@ plot_interaction(x = "X", z = "Z", y = "Y",
 plot_interaction(x = "X", z = "Z", y = "Y",
                  vals_z = c(-1,0,1), model = fit2)
 
-## multigroup approach
-oneInt2 <- oneInt %>% 
+### Johnson-Neyman plots
+plot_interaction(x = "X", z = "Z", y = "Y",
+                 vals_z = c(-1,0,1), model = fit3)
+fit3 <- modsem(model, data = oneInt2) #with less perfect data
+plot_jn(x = "X", z = "Z", y = "Y", model = fit2)
+
+## multigroup approach ----
+oneInt3 <- oneInt %>% 
   mutate(Z = rowMeans(across(starts_with("Z"))),
          group = cut(Z, breaks=c(-Inf, 0.5, 0.6, Inf), labels=c("low","middle","high"))) %>% 
   dplyr::select(-Z)
 
-head(oneInt2)
+head(oneInt3)
 
 model2 <- '
   # Measurement model
@@ -80,10 +96,10 @@ model2 <- '
   Y ~ X
 '
 
-multigroup1 <- sem(model2, data = oneInt2,
+multigroup1 <- sem(model2, data = oneInt3,
                    group = "group",
                    group.equal = c("intercepts", "loadings"))
-multigroup2 <- sem(model2, data = oneInt2,
+multigroup2 <- sem(model2, data = oneInt3,
                    group = "group",
                    group.equal = c("intercepts", "loadings", "regressions"))
 
